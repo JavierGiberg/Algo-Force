563fb88ba232f929953038eea74972e39112049c
https://github.com/JavierGiberg <gibergjavier@gmail.com>
2022-06-11 18:59:59 +0300

    SaveAll

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import NavBar from "./components/navBar/NavBar";
 import "./App.css";
 import React from "react";
-
 import "./App.css";
-
 import Home from "./components/Pages/Home";
-import Services from "./components/Pages/About";
-
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Contact from "./components/Pages/Contact";
-import About from "./components/Pages/About";
 import Cards from "./components/Card/Cards";
 function App() {
   return (
@@ -25,5 +20,4 @@ function App() {
     </>
   );
 }
-
 export default App;
