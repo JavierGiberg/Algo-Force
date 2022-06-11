089205d201fe13c899ff91aff5be7752cf000881
https://github.com/JavierGiberg <gibergjavier@gmail.com>
2022-06-11 11:55:14 +0300

    lastone

--- src/components/Pages/About.js
+++ src/components/Pages/About.js
@@ -5,55 +5,72 @@ export default function About() {
   return (
     <div>
       <div className="about">
-        <h1>About us</h1>
-        <p>
-          <h3>The special forces of the cryptographic market.</h3>
-          <b> Algo Force company</b> was founded by programmers and crypto
-          traders and set its destination to provide a solution to large clients
-          who wants to increase their crypto possessions.
-        </p>
-        <br />
-        <p>
-          We believe that crypto currency is making it way to the front of the
-          financial world and its value will only keep rising. <br />
-        </p>
+        <div className="about_text">
+          <h1>About us</h1>
+          <p>
+            <h3>The special forces of the cryptographic market.</h3>
+            <b> Algo Force company</b> was founded by programmers and crypto
+            traders and set its destination to provide a solution to large
+            clients who wants to increase their crypto possessions.
+          </p>
+          <br />
+          <p>
+            We believe that crypto currency is making it way to the front of the
+            financial world and its value will only keep rising. <br />
+          </p>
+        </div>
       </div>
 
       <div className="about">
-        <h1>How do we do it?</h1>
-        <p>
-          <b>We in Algo force</b> developed several trade <b>algorithms</b> who
-          analyze the market and trade by various strategies that can beat any
-          market situation and react properly .
-        </p>
-        <br />
-        <p>
-          Our algorithms are designed to different types of investments and
-          trade from a very solid trade with low risk to higher revenue but
-          higher risks.
+        <div className="about_text">
+          <h1>How do we do it?</h1>
           <p>
-            <b>
-              {" "}
-              We will continue to invest AI resources for company efficiency.
-            </b>
+            <b>We in Algo force</b> developed several trade <b>algorithms</b>{" "}
+            who analyze the market and trade by various strategies that can beat
+            any market situation and react properly .
           </p>
-          <br /> Algo Force is dedicated to be flexible and exclusive in order
-          to give any of our limited number of clients the best service with the
-          highest quality.
           <br />
-        </p>
+          <p>
+            Our algorithms are designed to different types of investments and
+            trade from a very solid trade with low risk to higher revenue but
+            higher risks.
+            <p>
+              <b>
+                {" "}
+                We will continue to invest <h3>Artificial intelligence</h3>{" "}
+                resources for company efficiency.
+              </b>
+            </p>
+            <br /> Algo Force is dedicated to be flexible and exclusive in order
+            to give any of our limited number of clients the best service with
+            the highest quality.
+            <br />
+            <br />
+            <br />
+          </p>
+          <p>
+            All of our algorithms are integrated and queen-level at different
+            levels. Thanks to this as time goes on our algorithms learn to
+            analyze the market more correctly and respond to the market in a
+            smarter and faster way.
+          </p>
+        </div>
       </div>
       <div className="about">
-        <h1> We understand</h1>
-        that one of the most important values for the investors is the personal
-        treatment and our full attention. That why we put service as our top
-        priority.
-        <br /> Any of our clients well have it own personal contact to keep in
-        touch as much needed including meetings face to face.
+        <div className="about_text">
+          <h1> We understand</h1>
+          that one of the most important values for the investors is the
+          personal treatment and our full attention. That why we put service as
+          our top priority.
+          <br /> Any of our clients well have it own personal contact to keep in
+          touch as much needed including meetings face to face.
+        </div>
       </div>
     </div>
   );
 }
+
+// All of our algorithms are integrated and queen-level at different levels. Thanks to this as time goes on our algorithms learn to analyze the market more correctly and respond to the market in a smarter and faster way.
 
 // Algo Force company was founded by programmers and crypto traders and set
 // its destination to provide a solution to large clients who wants to
